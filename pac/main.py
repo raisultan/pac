c5ee360f79a6b3871f5b0d3272a1a73763f635e0
raisultan <ki.xbozz@gmail.com>
2024-04-01 20:16:17 +0500

    fastapi added for API endpoints

--- pac/main.py
+++ pac/main.py
@@ -1,8 +1,9 @@
 import os
+from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
-from faststream import FastStream
-from faststream.kafka import KafkaBroker
+from fastapi import FastAPI
+from faststream.kafka.fastapi import KafkaRouter
 
 from pac.openai import OpenAIPACLLM
 from pac.vector_db.repository import VectorDB
@@ -16,23 +17,24 @@ vector_db = VectorDB(MilvusRepository())
 pac_llm_client = OpenAIPACLLM(api_key)
 pac = PAC(pac_llm_client, vector_db)
 
-broker = KafkaBroker('localhost:29092')
-app = FastStream(broker)
+router = KafkaRouter("localhost:29092")
 
 
-@app.on_startup
-async def setup_vector_db():
-    vector_db.connect()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with (router.lifespan_context(app)):
+        vector_db.connect()
+        yield
+        vector_db.disconnect()
 
 
-@app.on_shutdown
-async def shutdown_vector_db():
-    vector_db.disconnect()
-
-
-@broker.subscriber('test')
-@broker.publisher('test_out')
+@router.subscriber('test')
+@router.publisher('test_out')
 async def handle_ticket(ticket: Ticket):
     print(f'Handling ticket: {ticket}')
     event = await pac.categorize(ticket)
     return event
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
